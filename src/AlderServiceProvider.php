fe8da62599d0242e0d66f454c313e106a808c93e
Dmytro Kardava <kardava.d.g@fex.net>
2019-05-16 14:30:07 +0200

    test

---
+++ src/AlderServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Webcosmonauts\Alder;
+
+use Illuminate\Support\ServiceProvider;
+
+class AlderServiceProvider extends ServiceProvider
+{
+    /**
+     * Publishes configuration file.
+     *
+     * @return  void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/alder.php' => config_path('alder.php'),
+        ], 'alder-config');
+    }
+    /**
+     * Make config publishment optional by merging the config from the package.
+     *
+     * @return  void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/alder.php', 'alder');
+    }
+}
