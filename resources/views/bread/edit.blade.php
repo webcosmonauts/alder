7a3ed7b3678d23c561a9e615babe0d3bd73adecd
jarroot <jarroot1@gmail.com>
2019-06-06 12:38:06 +0300

    added component input

--- resources/views/bread/edit.blade.php
+++ resources/views/bread/edit.blade.php
@@ -83,15 +83,7 @@
 
                                 @if(!isset($field_modifiers->panel) || $field_modifiers->panel != 'right')
 
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"
-                                                  id="{{ $field_name }}">{{ $field_name }}</span>
-                                        </div>
-                                        <input type="text" class="form-control" name="{{ $field_name }}"
-                                               placeholder="{{ $field_name }}"
-                                               aria-label="{{ $field_name }}" aria-describedby="{{ $field_name }}">
-                                    </div>
+                                    @include('alder::components.input')
 
                                 @endif
 
@@ -119,58 +111,7 @@
                             @foreach($params->fields as $field_name => $field_modifiers)
                                 @if(isset($field_modifiers->panel) && $field_modifiers->panel == 'right')
 
-
-                                    @switch($field_modifiers->type)
-                                        @case('string')
-                                        <label for="{{ $field_name }}">{{ $field_name }}</label>
-                                        <div class="input-group mb-4">
-                                            <input type="text" name="{{ $field_name }}" id="{{ $field_name }}"
-                                                   class="form-control"
-                                                   placeholder="{{ $field_name }}"
-                                                   aria-label="{{ $field_name }}"
-                                                   aria-describedby="{{ $field_name }}">
-                                        </div>
-                                        @break
-
-
-                                        @case('date')
-                                        <label for="{{ $field_name }}">{{ $field_name }}</label>
-                                        <div class="input-group mb-4">
-                                            <input type="text" name="{{ $field_name }}" id="{{ $field_name }}"
-                                                   class="form-control datepicker"
-                                                   placeholder="{{ $field_name }}"
-                                                   aria-label="{{ $field_name }}"
-                                                   aria-describedby="{{ $field_name }}">
-                                        </div>
-                                        @break
-
-                                        @case('image')
-                                        <div>{{$field_name}}</div>
-                                        <div class="input-group mb-4">
-                                            <div class="custom-file">
-                                                <input type="file" name="{{ $field_name }}"
-                                                       accept="image/*"
-                                                       class="custom-file-input" id="{{ $field_name }}"
-                                                       aria-describedby="{{ $field_name }}">
-                                                <label class="custom-file-label" for="{{$field_name}}">Choose
-                                                    file</label>
-                                            </div>
-                                        </div>
-                                        @break
-
-                                        @case('dropdown')
-                                        <label for="{{$field_name}}"> {{$field_name}} </label>
-                                        <div class="input-group mb-4">
-                                            <select name="{{$field_name}}" id="{{$field_name}}"
-                                                    class="custom-select">
-                                                @foreach($field_modifiers->options as $option => $opt)
-                                                    <option value="{{$option}}">{{$opt}}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                        @break
-
-                                    @endswitch
+                                    @include('alder::components.input')
 
                                 @endif
                             @endforeach
---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,51 @@
+@switch($field_modifiers->type)
+    @case('string')
+    <label for="{{ $field_name }}">{{ $field_name }}</label>
+    <div class="input-group mb-4">
+        <input type="text" name="{{ $field_name }}" id="{{ $field_name }}"
+               class="form-control"
+               placeholder="{{ $field_name }}"
+               aria-label="{{ $field_name }}"
+               aria-describedby="{{ $field_name }}">
+    </div>
+    @break
+
+
+    @case('date')
+    <label for="{{ $field_name }}">{{ $field_name }}</label>
+    <div class="input-group mb-4">
+        <input type="text" name="{{ $field_name }}" id="{{ $field_name }}"
+               class="form-control datepicker"
+               placeholder="{{ $field_name }}"
+               aria-label="{{ $field_name }}"
+               aria-describedby="{{ $field_name }}">
+    </div>
+    @break
+
+    @case('image')
+    <div>{{$field_name}}</div>
+    <div class="input-group mb-4">
+        <div class="custom-file">
+            <input type="file" name="{{ $field_name }}"
+                   accept="image/*"
+                   class="custom-file-input" id="{{ $field_name }}"
+                   aria-describedby="{{ $field_name }}">
+            <label class="custom-file-label" for="{{$field_name}}">Choose
+                file</label>
+        </div>
+    </div>
+    @break
+
+    @case('dropdown')
+    <label for="{{$field_name}}"> {{$field_name}} </label>
+    <div class="input-group mb-4">
+        <select name="{{$field_name}}" id="{{$field_name}}"
+                class="custom-select">
+            @foreach($field_modifiers->options as $option => $opt)
+                <option value="{{$option}}">{{$opt}}</option>
+            @endforeach
+        </select>
+    </div>
+    @break
+
+@endswitch
