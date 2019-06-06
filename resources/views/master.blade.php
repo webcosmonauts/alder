db6fe69bb34c865c2ebf6deafa73862914cd1e69
jarroot <jarroot1@gmail.com>
2019-06-06 14:35:20 +0300

    master blade changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,12 +10,12 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/admin.min.js') }}"></script>
 
     <!-- Fonts -->
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin.min.css') }}" rel="stylesheet">
 
     @yield('scripts-head')
 </head>
@@ -23,8 +23,8 @@
 <!-- Page Wrapper -->
 <div id="wrapper">
     <!-- Sidebar -->
-    @yield('sidebar')
-    <!-- End of Sidebar -->
+@yield('sidebar')
+<!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
@@ -33,12 +33,12 @@
         <div id="content">
 
             <!-- Topbar -->
-            @yield('topbar')
-            <!-- End of Topbar -->
+        @yield('topbar')
+        <!-- End of Topbar -->
 
             <!-- Begin Page Content -->
-            @yield('main')
-            <!-- /.container-fluid -->
+        @yield('main')
+        <!-- /.container-fluid -->
 
         </div>
         <!-- End of Main Content -->
@@ -65,10 +65,18 @@
         crossorigin="anonymous"></script>
 <!-- Custom scripts for all pages-->
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 <!-- Custom fonts for this template-->
 <link href="{{ asset('css/all.min.css') }}" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-<link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+      rel="stylesheet">
+
 @yield('scripts-body')
 
 </body>
