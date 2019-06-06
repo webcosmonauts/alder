c7e584b266cfabc6f0d75aac46dddbe96ce98c0f
jarroot <jarroot1@gmail.com>
2019-06-06 15:28:50 +0300

    added datepiker, bootstrap 4, popper

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,9 @@ gulp.task('styles', gulp.series('sass'));
 gulp.task('scripts-admin', function () {
 	return gulp.src([
 		'jquery3-4-1.min.js',
+		'popper.min.js',
+		'bootstrap4.min.js',
+
 		'all.min.js',
 		'brands.js',
 		'Chart.bundle.js',
@@ -49,6 +52,8 @@ gulp.task('scripts-admin', function () {
 		'sb-admin-2.js',
 		'solid.js',
 		'v4-shims.js',
+
+		'datepicker.min.js',
 
 		'contact-form/contact-form.js',
 		'contact-form/contact-form-parser.js',
