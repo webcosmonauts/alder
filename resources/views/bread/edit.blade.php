41ec7dce86638d3c722f460887abd890bc257790
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-07 14:19:22 +0200

    prep edit bread for merge

--- resources/views/bread/edit.blade.php
+++ resources/views/bread/edit.blade.php
@@ -9,30 +9,21 @@
     <form action="{{ route("alder.$leaf_type->name.store") }}" method="POST">
         @csrf
 
-
         @php
-
             $right_panel_count = 0;
-            if(isset($params->fields)):
-
-            foreach($params->fields as $field_name => $field_modifiers) :
-                if(isset($field_modifiers->panel)) :
-                    if($field_modifiers->panel == 'right') :
-                        $right_panel_count++;
-                    endif;
-                endif;
-            endforeach;
-            endif;
-
+            if (isset($params->fields)) {
+                foreach ($params->fields as $field_name => $field_modifiers) {
+                    if (isset($field_modifiers->panel) && $field_modifiers->panel == 'right')
+                            $right_panel_count++;
+                }
+            }
         @endphp
 
         <div class="row">
-            <div class="col-lg-@php if($right_panel_count > 0)  echo '9'; else echo '12'; @endphp">
+            <div class="col-lg-{{ $right_panel_count > 0 ? '9' : '12' }}">
                 <div class="card shadow mb-4">
                     <div class="card-body">
-
                         @foreach(['title', 'slug', 'user_id'] as $field)
-
                             <label for="{{ $field }}">{{ $field }}</label>
                             <div class="input-group mb-4">
                                 <input type="text" name="{{ $field }}" id="{{ $field }}" class="form-control"
@@ -43,16 +34,14 @@
 
                         <label for="#content">Content</label>
                         <div class="input-group mb-4">
-                    <textarea type="text" rows="8" name="content" id="content" class="form-control"
+                        <textarea type="text" rows="8" name="content" id="content" class="form-control"
                               aria-label="content" aria-describedby="content" placeholder="Content"></textarea>
                         </div>
 
                         @if(isset($params->fields))
-
-
                             @foreach($params->fields as $field_name => $field_modifiers)
                             <!--
-                                 @if($field_modifiers->type == 'relation')
+                                @if($field_modifiers->type == 'relation')
                                     @switch($field_modifiers->relation_type)
                                         @case('hasOne')
                                             <div class="input-group mb-3">
@@ -76,51 +65,39 @@
                                         @default
                                     @endswitch
                                 @else
-
                                 @endif
                                     -->
 
-
                                 @if(!isset($field_modifiers->panel) || $field_modifiers->panel != 'right')
-
                                     @include('alder::components.input')
-
                                 @endif
-
-
                             @endforeach
                         @endif
 
                         <button type="submit" class="btn btn-success btn-icon-split">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-save"></i>
-                    </span>
+                            <span class="icon text-white-50">
+                              <i class="fas fa-save"></i>
+                            </span>
                             <span class="text">{{ __('alder::generic.save') }}</span>
                         </button>
                     </div>
                 </div>
             </div>
 
-
-        @if($right_panel_count > 0)
-
-            <!-- SIDEBAR -->
+            @if($right_panel_count > 0)
+                <!-- SIDEBAR -->
                 <div class="col-lg-3">
                     <div class="card shadow mb-4">
                         <div class="card-body">
                             @foreach($params->fields as $field_name => $field_modifiers)
                                 @if(isset($field_modifiers->panel) && $field_modifiers->panel == 'right')
-
                                     @include('alder::components.input')
-
                                 @endif
                             @endforeach
-
                         </div>
                     </div>
                 </div>
+            @endif
         </div>
-        @endif
     </form>
-
 @endsection
