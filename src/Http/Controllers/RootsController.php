016f1c25d6919322a0d51b62b0ee7d6a3a17e670
korovs444 <korovs444@gmail.com>
2019-06-09 11:43:57 +0200

    Update RootsController.php

--- src/Http/Controllers/RootsController.php
+++ src/Http/Controllers/RootsController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace Webcosmonauts\Alder\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -14,46 +15,59 @@ class RootsController extends BaseController
      *
      * @return View
      */
-    public function index() {
+    public function index()
+    {
         /* Get leaf type with custom modifiers */
         $root_types = RootType::with('roots')->get();
-        
+
         /* Get admin panel menu items */
         $admin_menu_items = Alder::getMenuItems();
-        
+
         return view('alder::roots.browse')->with([
             'root_types' => $root_types,
             'admin_menu_items' => $admin_menu_items,
         ]);
     }
-    
+
     /**
      * Get [C]RUD view
      * @param Request $request
      * @return View
      */
-    public function create(Request $request) {
-    
+    public function create(Request $request)
+    {
+
     }
-    
+
     /**
      * Create and store new leaf from request
      *
      * @param Request $request
      */
-    public function store(Request $request) {
-    
-    }
-    
-    public function edit() {
-    
-    }
-    public function update(Request $request) {
-
-
+    public function store(Request $request)
+    {
 
     }
-    public function destroy(Request $request, $id) {
-    
+
+    public function edit()
+    {
+
+    }
+
+    public function update(Request $request)
+    {
+
+        foreach ($request->except(['_token']) as $key => $value) {
+            Alder::setRootValue($key, $value);
+            echo 'Updated' . $key . '<br>';
+
+            return redirect()->back()->with(['success' => 'Settings is save']);
+        }
+
+    }
+
+    public function destroy(Request $request, $id)
+    {
+
     }
 }
