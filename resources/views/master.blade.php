2562afbadb0736f201bb248e81dc72ddc02ecaeb
jarroot <jarroot1@gmail.com>
2019-06-10 15:37:54 +0300

    added links to css, js in master blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -64,8 +64,12 @@
 
 
 @yield('scripts-body')
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"
+        integrity="sha256-H3cjtrm/ztDeuhCN9I4yh4iN2Ybx/y1RM7rMmAesA0k="
+        crossorigin="anonymous"></script>
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
+<link href="{{ asset('css/all.min.css') }}" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
       rel="stylesheet">
 <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
