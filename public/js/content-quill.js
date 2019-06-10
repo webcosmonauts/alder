913fdef18d44999ad0401a099bd2abbf650dfa73
jarroot <jarroot1@gmail.com>
2019-06-10 16:23:38 +0300

    added quill in edit blade

---
+++ public/js/content-quill.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+	var toolbarOptions = [
+		['bold', 'italic', 'underline'],        // toggled buttons
+		[{'list': 'ordered'}, {'list': 'bullet'}],
+		['blockquote', 'code-block'],
+
+		//[{'header': 1}, {'header': 2}, {'header': 3}, {'header': 4}],               // custom button values
+
+		[{'script': 'sub'}, {'script': 'super'}],      // superscript/subscript
+		[{'indent': '-1'}, {'indent': '+1'}],          // outdent/indent
+		[{'direction': 'rtl'}],                         // text direction
+
+		[{'header': [1, 2, 3, 4, 5, 6, false]}],
+
+		[{'color': []}, {'background': []}],          // dropdown with defaults from theme
+		[{'font': []}],
+		[{'align': []}],
+
+		['clean']                                         // remove formatting button
+	];
+
+	// QUILL INIT
+	if ($('#quill').length) {
+		var quill = new Quill('#quill', {
+			theme: 'snow',
+			modules: {
+				toolbar: toolbarOptions
+			}
+		});
+
+
+		$('form').on('submit', function (e) {
+			$('[name=content]').val(quill.container.firstChild.innerHTML);
+		});
+	}
+
+	/* this for fix bug when click on toolbar dropdown */
+	$('body').on('click', '.ql-formats', function (e) {
+		e.stopPropagation();
+	});
+});
