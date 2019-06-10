6c77e9e05c8b63fc3115c811d3625099de4c8094
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-10 10:48:57 +0200

    dashboard clean up

--- routes/alder.php
+++ routes/alder.php
@@ -1,7 +1,5 @@
 <?php
 
-Route::get("/{slug}","\Webcosmonauts\Alder\Http\Controllers\LeafController@index");
-
 Route::group(['prefix' => 'alder', 'middleware' => 'auth'], function () {
     Route::get('/', '\Webcosmonauts\Alder\Http\Controllers\DashboardController@index')->name('dashboard.index');
     
@@ -10,19 +8,19 @@ Route::group(['prefix' => 'alder', 'middleware' => 'auth'], function () {
         foreach (\Webcosmonauts\Alder\Models\LeafType::all() as $type) {
             Route::resource($type->name, '\Webcosmonauts\Alder\Http\Controllers\BranchBREADController');
         }
-
+        
         // Authentication Routes
         //Route::get('login', 'Auth\AlderLoginController@showLoginFormUsers')->name('login');
         //Route::post('users', 'Auth\AlderLoginController@checklogin');
         //Route::post('logout', 'Auth\AlderLoginController@logout')->name('logout');
-// Registration Routes
+        // Registration Routes
         //Route::get('users/registration', 'Auth\RegisterController@showRegistrationFormUsers')->name('register');
         //Route::post('users/registration', 'Auth\RegisterController@register');
-
+        
         // roots
         Route::get('roots', '\Webcosmonauts\Alder\Http\Controllers\RootsController@index')->name('roots.index');
         Route::post('roots/update', '\Webcosmonauts\Alder\Http\Controllers\RootsController@update')->name('roots.update');
-
+        
         // contact form
         Route::get('contact-form', 'ContactController@create')->name('contact-form');
         Route::post('contact-form', 'ContactController@store')->name('contact-form');
@@ -32,3 +30,5 @@ Route::group(['prefix' => 'alder', 'middleware' => 'auth'], function () {
     Route::get('media', '\Webcosmonauts\Alder\Http\Controllers\FileManagerController@index');
     Route::get('media-button', '\Webcosmonauts\Alder\Http\Controllers\FileManagerController@index_button');
 });
+
+Route::get("/{slug}","\Webcosmonauts\Alder\Http\Controllers\LeafController@index");
