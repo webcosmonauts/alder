88c7394c49d86993d4f7098d2825c051e851542c
jarroot <jarroot1@gmail.com>
2019-06-10 15:33:16 +0300

    file-uploadre right path to js

--- resources/views/file-uploader/uploader.blade.php
+++ resources/views/file-uploader/uploader.blade.php
@@ -1,7 +1,7 @@
 @extends('alder::layouts.main')
 @section('scripts-body')
     <link rel="stylesheet" href="{{asset('/vendor/file-manager/css/file-manager.css')}}">
-    <script src="{{asset('/vendor/file-manager/file-manager.min.js')}}"></script>
+    <script src="{{asset('/vendor/file-manager/file-manager.js')}}"></script>
 @endsection
 @section('content')
     <div style="height: 800px;">
