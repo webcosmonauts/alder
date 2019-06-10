3fd02b73e3256108f70921d0f334cfaec9f071af
jarroot <jarroot1@gmail.com>
2019-06-10 15:51:31 +0300

    datepicker in master.blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,6 +16,7 @@
 
     <!--  -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/datepicker.min.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
 
     @yield('scripts-head')
@@ -73,5 +74,9 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
       rel="stylesheet">
 <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+
+<script src="{{asset('js/datepicker.min.js')}}"></script>
+<script src="{{asset('js/admin.js')}}"></script>
 </body>
 </html>
