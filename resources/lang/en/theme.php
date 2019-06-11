8d3915ba9e9372980f6508b6af503f1c54787c1a
Cat Red <1rastawarrior1@gmail.com>
2019-06-11 09:36:07 +0200

    Added labels and default value translation

--- resources/lang/en/theme.php
+++ resources/lang/en/theme.php
@@ -3,4 +3,5 @@
 return [
     'footer_copyright'  => 'Made with <i class="voyager-heart"></i> by',
     'footer_copyright2' => 'Made with rum and even more rum',
+    'no_template_specified' => 'No template specified',
 ];
