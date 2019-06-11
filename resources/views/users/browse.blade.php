ed2fb08d6f8787baac78535d8f1e70961c0a63d8
korovs444 <korovs444@gmail.com>
2019-06-11 08:58:16 +0200

    Users controller, views and routs

--- resources/views/users/browse.blade.php
+++ resources/views/users/browse.blade.php
@@ -9,12 +9,12 @@
 
         <h1 class="h3 mb-0 text-gray-800"></h1>
         <!-- TODO: if can add new -->
-        {{--                <a href="{{ route("alder.$leaf_type->name.create") }}" class="btn btn-success btn-icon-split ml-3">--}}
-        {{--                    <span class="icon text-white-50">--}}
-        {{--                        <i class="fas fa-plus-circle"></i>--}}
-        {{--                    </span>--}}
-        {{--                    <span class="text">{{ __('alder::generic.add_new') . ' ' . Str::title(Str::singular(str_replace('-', ' ', $leaf_type->name))) }}</span>--}}
-        {{--                </a>--}}
+        <a href="{{ route("alder.users.create") }}" class="btn btn-success btn-icon-split ml-3">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-plus-circle"></i>
+                            </span>
+            <span class="text">{{ __('alder::generic.add_new')}}</span>
+        </a>
     </div>
 
     <div class="card shadow mb-4">
@@ -44,15 +44,14 @@
                                 </td>
                             @endforeach
                             <td class="text-right">
-                                <a href="{{ route("alder.$leaf_type->name.edit", $leaf->slug) }}" class="btn btn-primary btn-icon-split ml-3">
+                                <a href="{{ route("alder.users.show",  $leaf->id) }}" class="btn btn-primary btn-icon-split ml-3">
                                      <span class="icon text-white-50">
                                          <i class="fas fa-book-open"></i>
                                      </span>
                                     <span class="text">{{ __('alder::generic.read') }}</span>
                                 </a>
-
                                 <!-- TODO if can edit -->
-                                <a href="" class="btn btn-warning btn-icon-split ml-3">
+                                <a href="{{ route("alder.users.edit",  $leaf->id) }}" class="btn btn-warning btn-icon-split ml-3">
                                      <span class="icon text-white-50">
                                          <i class="fas fa-edit"></i>
                                      </span>
@@ -60,7 +59,7 @@
                                 </a>
 
                                 <!-- TODO if can delete -->
-                                <a href="" class="btn btn-danger btn-icon-split ml-3">
+                                <a href="{{ route("alder.users.destroy",  $leaf->id) }}" class="btn btn-danger btn-icon-split ml-3">
                                      <span class="icon text-white-50">
                                          <i class="fas fa-trash-alt"></i>
                                      </span>
--- routes/alder.php
+++ routes/alder.php
@@ -26,6 +26,11 @@ Route::group(['prefix' => 'alder', 'middleware' => 'auth'], function () {
         Route::post('contact-form', 'ContactController@store')->name('contact-form');
 
         Route::get('users', '\Webcosmonauts\Alder\Http\Controllers\UsersController@index')->name('Users.index');
+        Route::get('users/edit/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@edit')->name('users.edit');
+        Route::put('users/edit/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@update')->name('users.update');
+        Route::get('users/read/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@show')->name('users.show');
+        Route::get('users/destroy/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@destroy')->name('users.destroy');
+        Route::get('users/create', '\Webcosmonauts\Alder\Http\Controllers\UsersController@create')->name('users.create');
 
     });
 
