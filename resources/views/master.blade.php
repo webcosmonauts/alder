11258a6998cd8439c4741792decedd538827fc7a
jarroot <jarroot1@gmail.com>
2019-06-12 19:52:53 +0300

    master blade added custom-admin.css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,10 +19,11 @@
     <script src="{{ asset('js/app.js') }}"></script>
 
     <link href="{{ asset('css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/custom-admin.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
           rel="stylesheet">
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+
     <link rel="stylesheet" href="{{asset('css/icheck.css')}}">
     <link href="{{ asset('css/datepicker.min.css') }}" rel="stylesheet">
 
@@ -79,6 +80,7 @@
 
 
 <!-- My custom stuff -->
+
 <!-- ICheck -->
 <script src="{{asset('js/icheck.min.js')}}"></script>
 
