4b3d70898518ee2dd8e1c9a1cc9d33ac8460d93c
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-13 08:46:41 +0200

    action buttons moved to components

--- resources/views/bread/LCMs/browse.blade.php
+++ resources/views/bread/LCMs/browse.blade.php
@@ -32,28 +32,7 @@
                             <td>{{ $LCM->group_title }}</td>
                             <td>{{ $LCM->created_at }}</td>
                             <td class="text-right">
-                                <a href="{{ route("alder.LCMs.show", $LCM->slug) }}" class="btn btn-primary btn-icon-split ml-3">
-                                    <span class="icon text-white-50">
-                                        <i class="fas fa-book-open"></i>
-                                    </span>
-                                    <span class="text">{{ __('alder::generic.read') }}</span>
-                                </a>
-
-                                <!-- TODO if can edit -->
-                                <a href="{{ route("alder.LCMs.edit", $LCM->slug) }}" class="btn btn-warning btn-icon-split ml-3">
-                                    <span class="icon text-white-50">
-                                        <i class="fas fa-edit"></i>
-                                    </span>
-                                    <span class="text">{{ __('alder::generic.edit') }}</span>
-                                </a>
-
-                                <!-- TODO if can delete -->
-                                <a href="{{ route("alder.LCMs.destroy", $LCM->slug) }}" class="btn btn-danger btn-icon-split ml-3">
-                                    <span class="icon text-white-50">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </span>
-                                    <span class="text">{{ __('alder::generic.delete') }}</span>
-                                </a>
+                                @include('alder::components.actions', ['route' => 'LCMs', 'param' => $LCM->slug])
                             </td>
                         </tr>
                     @endforeach
