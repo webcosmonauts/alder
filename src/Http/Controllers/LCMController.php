9f1322ca71dfda7655d1fe4cb19b374c00ed334b
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-13 13:26:24 +0200

    LCM CRUD

--- src/Http/Controllers/LCMController.php
+++ src/Http/Controllers/LCMController.php
@@ -2,6 +2,7 @@
 
 namespace Webcosmonauts\Alder\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Webcosmonauts\Alder\Facades\Alder;
 use Webcosmonauts\Alder\Models\LeafCustomModifier;
@@ -34,7 +35,32 @@ class LCMController extends BaseController
 
     public function store(Request $request)
     {
-
+        return DB::transaction(function () use ($request) {
+            try {
+                $LCM = new LeafCustomModifier();
+                foreach (['title', 'slug', 'group_title', 'group_slug'] as $field)
+                    $LCM->$field = $request->$field;
+                $LCM->modifiers = json_decode($request->data);
+                $LCM->save();
+            
+                return Alder::returnRedirect(
+                    $request->ajax(),
+                    __('alder::generic.successfully_created') . " $LCM->title",
+                    route("alder.LCMs.index"),
+                    true,
+                    'success'
+                );
+            } catch (Exception $e) {
+                DB::rollBack();
+                return Alder::returnResponse(
+                    $request->ajax(),
+                    __('alder::messages.processing_error'),
+                    false,
+                    'danger',
+                    $e->getMessage()
+                );
+            }
+        });
     }
 
     public function edit(Request $request)
@@ -46,13 +72,60 @@ class LCMController extends BaseController
 
     }
 
-    public function update(Request $request)
+    public function update(Request $request, $param)
     {
-
+        return DB::transaction(function () use ($request, $param) {
+            try {
+                if (is_int($param))
+                    $LCM = LeafCustomModifier::findOrFail($param);
+                else
+                    $LCM = LeafCustomModifier::where('slug', $param)->firstOrFail();
+                
+                foreach (['title', 'slug', 'group_title', 'group_slug'] as $field)
+                    $LCM->$field = $request->$field;
+                $LCM->modifiers = json_decode($request->data);
+                $LCM->save();
+            
+                return Alder::returnRedirect(
+                    $request->ajax(),
+                    __('alder::generic.successfully_updated') . " $LCM->title",
+                    route("alder.LCMs.index"),
+                    true,
+                    'success'
+                );
+            } catch (Exception $e) {
+                DB::rollBack();
+                return Alder::returnResponse(
+                    $request->ajax(),
+                    __('alder::messages.processing_error'),
+                    false,
+                    'danger',
+                    $e->getMessage()
+                );
+            }
+        });
     }
 
-    public function destroy(Request $request)
+    public function destroy(Request $request, $param)
     {
-
+        if (is_int($param))
+            $LCM = LeafCustomModifier::findOrFail($param);
+        else
+            $LCM = LeafCustomModifier::where('slug', $param)->firstOrFail();
+        
+        return
+            $LCM->delete()
+                ? Alder::returnResponse(
+                $request->ajax(),
+                __('alder::messages.processing_error'), // todo deleted successfully
+                true,
+                'success'
+            )
+                : Alder::returnResponse(
+                $request->ajax(),
+                __('alder::messages.processing_error'),
+                false,
+                'danger'
+            );
     }
 }
