7bcfa26db4108cb1eb8abba32efa1e8aeb21bbfd
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-20 13:21:51 +0200

    user model update

--- src/Models/User.php
+++ src/Models/User.php
@@ -10,6 +10,32 @@ class User extends Authenticatable
 {
     use Notifiable;
     
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    
+    const ADMIN_TYPE = 'admin';
+    const DEFAULT_TYPE = 'user';
+    
+    
+    public function isAdmin(){
+        return $this->type === self::ADMIN_TYPE;
+    }
+    
     public function getFullNameAttribute() {
         return $this->name . ' ' . $this->surname;
     }
