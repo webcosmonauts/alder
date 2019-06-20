04cf76ba0ce0cbaee2750193a1cdefe3165a5118
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-20 13:21:26 +0200

    locale fix

--- src/Http/Middleware/LocaleSwitcher.php
+++ src/Http/Middleware/LocaleSwitcher.php
@@ -6,10 +6,12 @@ use Illuminate\Support\Facades\App;
 class LocaleSwitcher
 {
     public function handle($request, \Closure $next) {
-        App::setLocale(session('locale'));
-        if (session()->has('locale') && in_array(session('locale'), config('translatable.locales'))) {
+        if (!session()->has('locale'))
+            session()->put('locale', 'en');
+        
+        if (session()->has('locale') && in_array(session('locale'), config('translatable.locales')))
             App::setLocale(session('locale'));
-        }
+        
         return $next($request);
     }
 }
