408afde7fbcacda0eda4af800c661247ee72cb4e
jarroot <jarroot1@gmail.com>
2019-06-20 09:58:25 +0300

    #locale-group

--- resources/views/components/locale-switcher.blade.php
+++ resources/views/components/locale-switcher.blade.php
@@ -1,4 +1,4 @@
-<div class="btn-group ml-auto" role="group">
+<div id="locale-group" class="btn-group ml-auto" role="group">
     @foreach(config('translatable.locales') as $locale)
         <a href="/setlocale/{{ $locale }}" class="btn btn-primary {{ $locale == session('locale') ? 'disabled' : '' }}">
             <span class="text">{{ strtoupper($locale) }}</span>
