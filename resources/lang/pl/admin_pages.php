ac26098b8ca03a7e9f1d31656fbc19556f0254f9
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-21 14:19:37 +0200

    langs update

---
+++ resources/lang/pl/admin_pages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'login_welcome' => 'Witamy w Alder CMS',
+    'forgot_password' => 'Nie pamiętasz hasła?',
+    'reset_password' => 'Zresetuj hasło',
+    'enter_email' => 'Wprowadź adres e-mail',
+    'have_account' => 'Posiadasz już konto? Zaloguj Się!',
+    'reset_request_description' => 'Wpisz poniżej swój adres e-mail, a wyślemy Ci link do zresetowania hasła!',
+];
