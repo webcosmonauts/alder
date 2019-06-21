e8afa32f0a03e0cf82c1c97984ea12a130c88ec6
korovs444 <korovs444@gmail.com>
2019-06-21 11:52:08 +0200

    Update alder.php

--- routes/alder.php
+++ routes/alder.php
@@ -8,14 +8,14 @@ Route::get('setlocale/{locale}', function ($locale) {
 });
 
 Route::group(['middleware' => 'locale-switcher'], function () {
-    
+
     Route::group(['prefix' => 'alder', 'middleware' => 'AlderGuard'], function () {
         Route::get('/', '\Webcosmonauts\Alder\Http\Controllers\DashboardController@index')->name('dashboard.index');
-        
+
         Route::name('alder.')->group(function () {
             // LCM
             Route::resource('LCMs', '\Webcosmonauts\Alder\Http\Controllers\LCMController');
-            
+
             // leaf types
             foreach (\Webcosmonauts\Alder\Models\LeafType::all() as $type) {
                 Route::resource($type->slug, '\Webcosmonauts\Alder\Http\Controllers\BranchBREADController');
@@ -31,7 +31,7 @@ Route::group(['middleware' => 'locale-switcher'], function () {
             Route::get('contact-forms/read/{slug}', '\Webcosmonauts\Alder\Http\Controllers\ContactController@read')->name('contact-forms.read');
             Route::get('contact-forms/edit/{slug}', '\Webcosmonauts\Alder\Http\Controllers\ContactController@edit_mailer')->name('contact-forms.edit_mailer');
             Route::post('contact-forms/pars_mailer/{slug}', '\Webcosmonauts\Alder\Http\Controllers\ContactController@pars_mailer')->name('contact-forms.pars_mailer');
-            
+
             //users
             Route::get('users', '\Webcosmonauts\Alder\Http\Controllers\UsersController@index')->name('users.index');
             Route::get('users/edit/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@edit')->name('users.edit');
@@ -40,27 +40,34 @@ Route::group(['middleware' => 'locale-switcher'], function () {
             Route::get('users/destroy/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@destroy')->name('users.destroy');
             Route::get('users/create', '\Webcosmonauts\Alder\Http\Controllers\UsersController@create')->name('users.create');
             Route::post('users/store', '\Webcosmonauts\Alder\Http\Controllers\UsersController@store')->name('users.store');
-            
+
             //profile
             Route::get('profile', '\Webcosmonauts\Alder\Http\Controllers\ProfileController@index')->name('profile.index');
-            
+
             //Register
-            
+
             //Appearance
             Route::get('appearance', '\Webcosmonauts\Alder\Http\Controllers\TemplateControllers\TemplateController@appearance')->name('appearance.index');
-            
+
             //Themes show
             Route::get('themes', '\Webcosmonauts\Alder\Http\Controllers\TemplateControllers\TemplateController@showThemes')->name('themes.index');
             //Select active theme
             Route::post('themes', '\Webcosmonauts\Alder\Http\Controllers\TemplateControllers\TemplateController@selectActiveTheme')->name('themes.update');
-            
+
             //Menus
             Route::get('menus/create', '\Webcosmonauts\Alder\Http\Controllers\MenuController@menuCreate')->name('menus.create');
-            
+            Route::put('menus/{menu}', '\Webcosmonauts\Alder\Http\Controllers\MenuController@update')->name('menus.update');
+            Route::get('menus/{menu}/edit', '\Webcosmonauts\Alder\Http\Controllers\MenuController@editMenu')->name('menus.edit');
+            Route::get('menus/{menu}', '\Webcosmonauts\Alder\Http\Controllers\MenuController@show')->name('menus.show');
+            Route::post('menus', '\Webcosmonauts\Alder\Http\Controllers\MenuController@store')->name('menus.store');
+            Route::delete('menus/{menu}', '\Webcosmonauts\Alder\Http\Controllers\MenuController@deleteMenu')->name('menus.destroy');
+
             //Select active theme
             Route::get('theme-settings', '\Webcosmonauts\Alder\Http\Controllers\TemplateControllers\ViewHierarchyController@index')->name('theme_settings.index');
             Route::post('theme-settings', '\Webcosmonauts\Alder\Http\Controllers\TemplateControllers\ViewHierarchyController@setViewsHierarchy')->name('theme_settings.update');
-            
+            Route::post('theme-settings/save', '\Webcosmonauts\Alder\Http\Controllers\TemplateControllers\ViewHierarchyController@saveRoots')->name('saveRoots');
+
+
             //Widgets builder
             Route::get('widgets', '\Webcosmonauts\Alder\Http\Controllers\WidgetsController@index')->name('widgets.index');
             Route::post('widgets', '\Webcosmonauts\Alder\Http\Controllers\WidgetsController@update')->name('widgets.update');
@@ -81,14 +88,14 @@ Route::group(['middleware' => 'locale-switcher'], function () {
 
         });
     });
-    
+
     //App\Http\Controllers
     Route::get('/register', '\Webcosmonauts\Alder\Http\Controllers\RegisterController@index')->name('register.index');
     Route::get('activation', '\Webcosmonauts\Alder\Http\Controllers\RegisterController@activation')->name('register.activation');
     Route::post('/register', '\Webcosmonauts\Alder\Http\Controllers\RegisterController@save')->name('register.save');
     Route::get('/verificated', '\Webcosmonauts\Alder\Http\Controllers\RegisterController@verificated')->name('register.verificated');
-    
+
     Route::get("/{slug}","\Webcosmonauts\Alder\Http\Controllers\LeafController@index");
-    
+
     //Route::get("/{leaf_type}/{slug}","\Webcosmonauts\Alder\Http\Controllers\LeafController@leafTypeShow");
 });
