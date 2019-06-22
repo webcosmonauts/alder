87dd08680ad8719741b4c4840b8a37dfb082ba52
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-22 17:05:33 +0200

    delete message en translation fix

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,6 +2,6 @@
 
 return [
     'processing_error' => 'There was an error processing your request',
-    'delete_successful' => 'Deleted successfully',
-    'delete_successful_named' => 'Successfully deleted :name',
+    'delete_successfully' => 'Deleted successfully',
+    'delete_successfully_named' => 'Successfully deleted :name',
 ];
