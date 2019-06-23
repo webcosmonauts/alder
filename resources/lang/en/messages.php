beb1288d31e9cd356f6291af68a5dbc27845ee03
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-23 11:53:52 +0200

    lang 'empty field message'

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,4 +4,5 @@ return [
     'processing_error' => 'There was an error processing your request',
     'delete_successfully' => 'Deleted successfully',
     'delete_successfully_named' => 'Successfully deleted :name',
+    'field_empty' => 'Field :name cannot be empty',
 ];
--- resources/lang/pl/messages.php
+++ resources/lang/pl/messages.php
@@ -4,4 +4,5 @@ return [
     'processing_error' => 'Podczas przetwarzania żądania wystąpił błąd',
     'delete_successfully' => 'Skutecznie usunięte',
     'delete_successfully_named' => 'Pomyślnie usunięto :name',
+    'field_empty' => 'Pole :name nie może być puste',
 ];
