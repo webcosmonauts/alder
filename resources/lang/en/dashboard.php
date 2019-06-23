9d8250540d9dcbd28fbb2c3031e261f8f58d4dd3
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-23 17:52:36 +0200

    dashboard rework

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'singular' => 'Dashboard',
+    'total' => 'Total',
+    'last_updated' => 'Last updated',
+    'add_new_page' => 'Add new page',
+    'add_new_post' => 'Add new post',
+    'add_new_user' => 'Add new user',
+    'users_total' => 'Total users',
+    'users_total_active' => 'Total active users',
+];
