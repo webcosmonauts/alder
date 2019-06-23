9f9bd53763504b04dd9dd42c38f25c908a6cb5b4
Dmytro Kardava <kardava.d.g@fex.net>
2019-06-23 17:57:14 +0200

    default locale to pl + admin pages title fix

--- src/Http/Middleware/LocaleSwitcher.php
+++ src/Http/Middleware/LocaleSwitcher.php
@@ -7,7 +7,7 @@ class LocaleSwitcher
 {
     public function handle($request, \Closure $next) {
         if (!session()->has('locale'))
-            session()->put('locale', 'en');
+            session()->put('locale', 'pl');
         
         if (session()->has('locale') && in_array(session('locale'), config('translatable.locales')))
             App::setLocale(session('locale'));
