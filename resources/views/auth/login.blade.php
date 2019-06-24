369e587afb2177b2fde56eafbfa763e33b4d3cb4
jarroot <jarroot1@gmail.com>
2019-06-24 12:07:12 +0300

    login thumbnail changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -90,5 +90,6 @@
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
 <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/custom-admin.css') }}" rel="stylesheet">
 </body>
 </html>
