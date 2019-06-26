316c7a642c21a96590db32224275b0ede6e19954
korovs444 <korovs444@gmail.com>
2019-06-26 12:38:27 +0200

    Update alder.php

--- routes/alder.php
+++ routes/alder.php
@@ -49,7 +49,7 @@ Route::group(['middleware' => 'locale-switcher'], function () {
             Route::get('users/edit/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@edit')->name('users.edit');
             Route::put('users/edit/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@update')->name('users.update');
             Route::get('users/read/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@show')->name('users.show');
-            Route::get('users/destroy/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@destroy')->name('users.destroy');
+            Route::delete('users/destroy/{user}', '\Webcosmonauts\Alder\Http\Controllers\UsersController@destroy')->name('users.destroy');
             Route::get('users/create', '\Webcosmonauts\Alder\Http\Controllers\UsersController@create')->name('users.create');
             Route::post('users/store', '\Webcosmonauts\Alder\Http\Controllers\UsersController@store')->name('users.store');
 
