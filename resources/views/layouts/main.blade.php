1ba8a98d6a86f35c2956a3f0ab745a79e2a6154f
jarroot <jarroot1@gmail.com>
2019-06-27 20:06:53 +0300

    Ebanye strelochki

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -77,16 +77,6 @@
 @section('topbar')
     <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
 
-        <div class="navbar-wrapper">
-            <div class="navbar-minimize">
-                <button id="minimizeSidebar" class="btn btn-just-icon btn-white btn-fab btn-round">
-                    <i class="material-icons text_align-center visible-on-sidebar-regular">more_vert</i>
-                    <i class="material-icons design_bullet-list-67 visible-on-sidebar-mini">view_list</i>
-                </button>
-            </div>
-        </div>
-
-
         <!-- Topbar Navbar -->
         <ul class="navbar-nav ml-auto">
 
